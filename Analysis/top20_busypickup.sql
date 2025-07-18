f6a4c0e4b1369ceeb961ddd86a8f3cef1c0e4489
harsha-learns <harsha_learns@outlook.com>
2025-07-18 00:58:39 -0400

    Analysis :  Added new SQL analysis files for taxi data visualization.

---
+++ Analysis/top20_busypickup.sql
@@ -0,0 +1,8 @@
+SELECT lo.location_id, COUNT(*) as trip_count FROM `nyctaxidata.FACT_TAXI_TRIP` ft
+JOIN `nyctaxidata.dim_location` lo
+ON lo.location_id = ft.pickup_location_id
+GROUP BY location_id
+ORDER BY trip_count DESC
+LIMIT 20
+
+
