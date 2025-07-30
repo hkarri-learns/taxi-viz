1dad6ee77acc3a6d8840cdfa7485dc1c15527d17
Harsha <hkarri.learns@gmail.com>
2025-07-30 10:38:03 -0400

    Create datetime_insert.sql

---
+++ Dev/Modeling/Load/datetime_insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO nyctaxidata.dim_datetime (
+  datetime_id, pickup_datetime, dropoff_datetime,
+  pickup_hour, pickup_day, pickup_month,
+  pickup_year, pickup_weekday
+)
+SELECT DISTINCT
+  FORMAT_TIMESTAMP('%Y%m%d%H%M%S', tpep_pickup_datetime) AS datetime_id,
+  DATETIME(tpep_pickup_datetime) as pickup_datetime,
+  DATETIME(tpep_dropoff_datetime) as dropoff_datetime,
+  EXTRACT(HOUR FROM tpep_pickup_datetime),
+  EXTRACT(DAY FROM tpep_pickup_datetime),
+  EXTRACT(MONTH FROM tpep_pickup_datetime),
+  EXTRACT(YEAR FROM tpep_pickup_datetime),
+  FORMAT_TIMESTAMP('%A', tpep_pickup_datetime)
+FROM `nyctaxidata.taxialyx`
